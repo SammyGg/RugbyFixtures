22199cf231f87f28b89997843d5a01bfc6190ce6
SammyGg <samuelgascoyne@outlook.com>
2024-05-13 19:20:28 +0100

    Page

--- rfpj/rfsite/views.py
+++ rfpj/rfsite/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from datetime import datetime
 
 def welcome(request):
-    return HttpResponse("Welcome to the Rugby Fixture finder")
+    return render(request, "rfsite/welcome.HTML")
 # Create your views here.
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
