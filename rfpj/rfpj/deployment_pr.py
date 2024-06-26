ed9e00409a08a16ca9c600856e3a90dbaffcd17a
mjgascoyne <mjgascoyne@hotmail.com>
2024-06-26 19:31:13 +0100

    dep_07_allowedhosts

--- rfpj/rfpj/deployment_pr.py
+++ rfpj/rfpj/deployment_pr.py
@@ -4,7 +4,7 @@ from .settings import *
 from .settings import BASE_DIR
 
 # print ("Starting pr_dep")
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = ['169.254.129.2', os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 
 DEBUG = False
